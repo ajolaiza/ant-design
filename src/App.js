7480cf5b2d2766b36ac288ac0ce5595308607da0
TECH INVO <ankitkarki35@gmail.com>
2020-02-25 16:19:13 +0545

    form simpler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+
+// import { Layout } from 'antd';
+// import { Typography } from 'antd';
+// import { Avatar } from 'antd';
+// import { Menu } from 'antd';
+
+// const{Header,Footer,Sider,Content}=Layout;
+// const{Title}=Typography
+// const { SubMenu } = Menu;
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <Layout>
+
+//      <Header style={{padding:'10px'}}>
+//      <Avatar style={{float:'right'}} src='/pic1.png'  />
+//      <Title style={{color:'white'}}level={3}>Guidelines and Resources</Title>
+//      </Header>
+     
+//       <Layout>
+//       <Sider style={{background:'red'}}>
+//          slider
+//        </Sider> 
+//       <Menu>
+//         <Menu.item key='Dashboard'>
+//           Dashboard
+//         </Menu.item>
+//         <SubMenu>
+//           <Menu.ItemGroup title="About us">
+//             <Menu.Item key='location 1'>
+//               Location
+//             </Menu.Item>
+//             <Menu.Item key='location 2'>
+//               Location2
+//             </Menu.Item>
+
+
+//           </Menu.ItemGroup>
+
+//         </SubMenu>
+//       </Menu>
+
+//       </Layout>
+//       <Layout>
+        
+//        <Content>Content</Content>
+//        <Footer>Footer</Footer>
+//      </Layout>
+// </Layout>
+     
+      
+
+    
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, {Component} from 'react'
 import './App.css';
+import Form from './Form'
+class App extends Component{
+  render(){
+    return(
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <Form/>
+
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
